15c0014fe8eedca5b55f50e0f96e66fe3e46b618
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-22 22:59:15 +0800

    list creation

--- exercise 5.py
+++ exercise 5.py
@@ -1,2 +1,6 @@
 #Exercise 5: Check if the first and last number of a list is the same
 #Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
+
+first_list = [10, 20, 30, 40, 10]
+second_list = [75, 65, 35, 75, 30]
+
